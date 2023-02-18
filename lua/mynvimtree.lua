ece670a3bc25864670821ff6bbdb4d97f5117018
Martín Ugarte <martin@fintual.com>
2023-02-18 10:46:02 -0300

    chore: add and configure nvim-tree

---
+++ lua/mynvimtree.lua
@@ -0,0 +1,48 @@
+local HEIGHT_RATIO = 0.9
+local WIDTH_RATIO = 0.7
+
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = "rounded",
+          relative = "editor",
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
+    width = 60,
+    number = true,
+    relativenumber = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+vim.keymap.set("n", "<C-t>", function()
+  return require("nvim-tree").focus(false, true)
+end, { silent = true, desc = "toggle nvim-tree" })
